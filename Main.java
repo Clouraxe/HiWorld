a3ee8b4313f48d1dfbfa8c8f12d2286dbffbe04d
clouraxe <clouraxe@gmail.com>
2023-10-22 13:41:50 +0300

    Added language support

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("yo world wassup");
+
+        if (args.length > 0 && args[0].toLowerCase().equals("-l")) {
+
+            String text;
+            switch (args[1]) {
+                case "ar":
+                text = "مرحبا بالعالم";
+                break;
+
+                case "en":
+                    text = "hello world!";
+                    break;
+                
+                    case "tr":
+                    text = "Selam Dünya";
+                    break;
+                
+                default:
+                    text = "Sorry this language is not supported yet";
+            }
+
+            System.out.println(text);
+        } else {
+            System.out.println("Usage: java Main.java -l (lang)");
+        }
     }
 }
